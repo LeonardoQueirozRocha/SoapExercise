8a9e862b60f3492ecaa36916de62ab2ba3281e7f
Leonardo Queiroz Rocha <leonardo.queiroz.rocha@outlook.com>
2022-06-11 11:26:05 -0300

    Formatação do retorno

--- src/Soap.App/Program.cs
+++ src/Soap.App/Program.cs
@@ -20,27 +20,29 @@ namespace Soap.App
             try
             {
                 var pessoa = await pessoaService.ProcurarPessoaAync("50");
-                Console.WriteLine(pessoa.Nome);
-                Console.WriteLine(pessoa.Ssn);
-                Console.WriteLine(pessoa.DataNascimento);
-                Console.WriteLine(pessoa.Idade);
-                Console.WriteLine(pessoa.Casa.Nome);
-                Console.WriteLine(pessoa.Casa.Estado);
-                Console.WriteLine(pessoa.Casa.Zip);
-                Console.WriteLine(pessoa.Escritorio.Nome);
-                Console.WriteLine(pessoa.Escritorio.Estado);
-                Console.WriteLine(pessoa.Escritorio.Zip);
-                Console.WriteLine(string.Join(", ", pessoa.CoresFavoritas));
+                Console.WriteLine("Nome: " + pessoa.Nome);
+                Console.WriteLine("SSN: " + pessoa.Ssn);
+                Console.WriteLine("Data de Nascimento: " + pessoa.DataNascimento);
+                Console.WriteLine("Idade: " + pessoa.Idade);
+                Console.WriteLine("Endereço residencial:");
+                Console.WriteLine("Cidade: " + pessoa.Casa.Nome);
+                Console.WriteLine("Estado: " + pessoa.Casa.Estado);
+                Console.WriteLine("Zip code: " + pessoa.Casa.Zip);
+                Console.WriteLine("Endereço do escritório:");
+                Console.WriteLine("Cidade: " + pessoa.Escritorio.Nome);
+                Console.WriteLine("Estado: " + pessoa.Escritorio.Estado);
+                Console.WriteLine("Zip code: " + pessoa.Escritorio.Zip);
+                Console.WriteLine("Cores favoritas: " + string.Join(", ", pessoa.CoresFavoritas));
 
                 Console.WriteLine("\n-------------------------------\n");
                 var cidade = await cidadeService.ProcurarCidadeAsync("10012");
-                Console.WriteLine(cidade.Nome);
-                Console.WriteLine(cidade.Estado);
-                Console.WriteLine(cidade.Zip);
+                Console.WriteLine("Cidade: " + cidade.Nome);
+                Console.WriteLine("Estado: " + cidade.Estado);
+                Console.WriteLine("Zip code: " + cidade.Zip);
 
                 Console.WriteLine("\n-------------------------------\n");
                 var missao = await missaoService.ObterDeclaracaoDeMissaoAsync();
-                Console.WriteLine(missao);
+                Console.WriteLine("Missão: " + missao);
             }
             catch (Exception ex)
             {
